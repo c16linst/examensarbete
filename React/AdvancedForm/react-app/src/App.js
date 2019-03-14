9a5ac251ceb400ff9b03e5ad5f6a66cf916583f3
Linda Strålberg <linda@Intern007.local>
2019-03-14 17:24:50 +0100

    Split Form component into smaller components (TableRow and Input)

--- React/AdvancedForm/react-app/src/App.js
+++ React/AdvancedForm/react-app/src/App.js
@@ -13,6 +13,133 @@ class Form extends Component {
   constructor(props) {
     super(props);
 
+    this.formRef = React.createRef();
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+
+    let form = this.formRef.current;
+    if(form.checkValidity()) form.submit();
+  }
+
+  renderTableRows() {
+    const rowContent = [
+      {
+        type: 'text',
+        placeholder: 'Anything',
+        label: 'Text'
+      },
+      {
+        type: 'email',
+        placeholder: 'example@test.com',
+        label: 'E-mail'
+      },
+      {
+        type: 'tel',
+        placeholder: '0701234567',
+        label: 'URL'
+      },
+      {
+        type: 'url',
+        placeholder: 'http://www.exampleweb.ex',
+        label: 'URL'
+      },
+      {
+        type: 'number',
+        placeholder: '12345',
+        label: 'Number'
+      },
+      {
+        type: 'password',
+        placeholder: 'Anything',
+        label: 'Password'
+      },
+      {
+        type: 'datetime-local',
+        placeholder: '2019-01-01T09:30',
+        label: 'Date-time'
+      },
+      {
+        type: 'month',
+        placeholder: '2019-01',
+        label: 'Month'
+      },
+      {
+        type: 'week',
+        placeholder: '2019-W01',
+        label: 'Week'
+      },
+      {
+        type: 'search',
+        placeholder: 'Anything',
+        label: 'Search'
+      }
+    ];
+
+    const tableRows = rowContent.map((tableRow) =>
+      <TableRow
+        key={tableRow.type}
+        type={tableRow.type}
+        placeholder={tableRow.placeholder}
+        label={tableRow.label} />
+    );
+
+    return tableRows;
+  }
+
+  render() {
+    let handleSubmit = this.handleSubmit;
+
+    return (
+      <form
+        style={{display: 'flex', flexDirection: 'column', width: '150px'}}
+        ref={this.formRef}
+        noValidate >
+        <table>
+          <tbody>
+            {this.renderTableRows()}
+          </tbody>
+        </table>
+        <Input
+          type="submit"
+          handleSubmit={handleSubmit.bind(this)} />
+
+      </form>
+    );
+  }
+}
+
+class TableRow extends Component {
+  render() {
+    let id = this.props.type;
+    if(id === 'datetime-local') id = 'date';
+
+    return (
+      <tr>
+        <td>
+        <label
+          htmlFor={this.props.type}
+          style={{display: 'inline-block', width: '120px', fontWeight: 'bold'}} >
+          {this.props.label}:
+        </label>
+        </td>
+        <td>
+          <Input
+            id={id}
+            type={this.props.type}
+            placeholder={this.props.placeholder} />
+
+        </td>
+      </tr>
+    );
+  }
+}
+
+class Input extends Component {
+  constructor(props) {
+    super(props);
+
     this.state = {
       username: '',
       email: '',
@@ -25,8 +152,6 @@ class Form extends Component {
       week: '',
       search: ''
     }
-
-    this.formRef = React.createRef();
   }
 
   handleInput(e) {
@@ -36,73 +161,33 @@ class Form extends Component {
     this.setState({ [name]: value });
   }
 
-  tablerow(type, placeholder, label, options) {
-    var width = '160px';
-    if(options === 'search') width = '168px';
-
-    var id = type;
-    if(options === 'date') id = 'date';
-
-    return (
-      <tr>
-        <td>
-        <label
-          htmlFor={type}
-          style={{display: 'inline-block', width: '120px', fontWeight: 'bold'}} >
-          {label}:
-        </label>
-        </td>
-        <td>
-          <input
-            type={type}
-            id={id}
-            name={type}
-            placeholder={placeholder}
-            value={this.state.name}
-            onChange={(e) => this.handleInput(e)}
-            style={{width: width}}
-            required
-          />
-        </td>
-      </tr>
-    );
-  }
-
-  handleSubmit(e) {
-    e.preventDefault();
-
-    let form = this.formRef.current;
-    if(form.checkValidity()) form.submit();
-  }
-
   render() {
-    return (
-      <form
-        style={{display: 'flex', flexDirection: 'column', width: '150px'}}
-        ref={this.formRef}
-        noValidate >
-        <table>
-          <tbody>
-            {this.tablerow('text', 'Anything', 'Text')}
-            {this.tablerow('email', 'example@test.com', 'E-mail')}
-            {this.tablerow('tel', '+4676 123 45 67', 'Telephone')}
-            {this.tablerow('url', 'http://www.exampleweb.ex', 'URL')}
-            {this.tablerow('number', '12345', 'Number')}
-            {this.tablerow('password', 'Anything', 'Password')}
-            {this.tablerow('datetime-local', '2019-01-01T09:30', 'Date-time', 'date')}
-            {this.tablerow('month', '2019-01', 'Month')}
-            {this.tablerow('week', '2019-W01', 'Week')}
-            {this.tablerow('search', 'Anything', 'Search', 'search')}
-          </tbody>
-        </table>
+    let width = '160px';
+    if(this.props.type === 'search') width = '168px';
+
+    if(this.props.type === 'submit') {
+      return (
         <input
           type="submit"
           id="submit-form"
           value="Register"
-          onClick={(e) => this.handleSubmit(e)}
+          onClick={(e) => this.props.handleSubmit(e)}
         />
-      </form>
-    );
+      );
+    } else {
+      return (
+        <input
+          type={this.props.type}
+          id={this.props.id}
+          name={this.props.type}
+          placeholder={this.props.placeholder}
+          value={this.state.name}
+          onChange={(e) => this.handleInput(e)}
+          style={{width: width}}
+          required
+        />
+      );
+    }
   }
 }
 
